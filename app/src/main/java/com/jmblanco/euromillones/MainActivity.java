da59e0f798bd8e353780cc4de2721419442bd7ac
jmblanco <jmblanco1985@gmail.com>
2021-06-15 15:13:43 +0200

    new project

---
+++ app/src/main/java/com/jmblanco/euromillones/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jmblanco.euromillones;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
